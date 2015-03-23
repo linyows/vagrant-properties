e663f536eca93f603a83dc92e5746956d15d9bc1
linyows <linyows@gmail.com>
2015-03-23 17:45:44 +0900

    remove enable-methods

--- lib/vagrant-properties.rb
+++ lib/vagrant-properties.rb
@@ -1,24 +1,12 @@
 require 'vagrant-properties/version'
 
 module VagrantPlugins
-  module Properties
-    class << self
-      def enable
-        @enabled = true
-      end
-
-      def enabled?
-        @enabled
-      end
-    end
-  end
-
   module Kernel_V2
     class Plugin < Vagrant.plugin('2')
       config('properties') do
         require File.expand_path('../vagrant-properties/config', __FILE__)
         Properties::Config
-      end if VagrantPlugins::Properties.enabled?
+      end
     end
   end
 end
